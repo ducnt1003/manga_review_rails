2336648f31edfcf3d5f294e99199d596077c25f0
phonhay103 <phonhay103@gmail.com>
2021-12-12 12:40:38 -0500

    Manga6

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -64,6 +64,6 @@ class AuthorsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def author_params
-      params.require(:author).permit(:name, :birthday, :story)
+      params.require(:author).permit(:name, :birthday, :story, :image)
     end
 end
